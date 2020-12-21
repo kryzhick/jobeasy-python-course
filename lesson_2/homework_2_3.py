46579bb1c2bfa3df13ca0fcb13e75ab6be5c3c18
kryzhick <kryzhick@icloud.com>
2020-12-20 21:11:29 -0500

    git init commit

--- lesson_2/homework_2_3.py
+++ lesson_2/homework_2_3.py
@@ -3,5 +3,6 @@
 # !important you should save only number to result_temperature. Formula (32°F − 32) × 5/9 = 0°C
 
 # type your code here
-temperature_fahrenheit = None
-result_temperature = None
+temperature_fahrenheit = int(input('What is temperature right now?:'))
+result_temperature = (temperature_fahrenheit - 32) * 5 / 9
+print(f'In Celsius it will be: {result_temperature}')
