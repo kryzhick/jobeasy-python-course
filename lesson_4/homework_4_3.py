6bb9c3c62e38649b52ef035d5ca2a99c9ebe651c
kryzhick <kryzhick@icloud.com>
2020-12-28 21:54:14 -0500

    git init commit

--- lesson_4/homework_4_3.py
+++ lesson_4/homework_4_3.py
@@ -4,20 +4,39 @@
 # Write code, which will count how many times your character is included in your string.
 # Save result to result_1 variable
 
-string_1 = None
-char_1 = None
-result_1 = None
-
-
-# Enter a random number and save it in variable number_1. Then create code to multiply all the digits together
-# and save result in the result_2 variable.
-
-number_1 = None
-result_2 = None
+# string_1 = 'Python is the best language in the world'
+# char_1 = 't'
+#
+# index = 0
+# # counter = 0
+# #
+# # while index < len(string_1):
+# #     if string_1[index] == char_1:
+# #         counter += 1
+# #     index += 1
+# # result_1 = counter
+# # print(result_1)
+#
+#
+# # Enter a random number and save it in variable number_1. Then create code to multiply all the digits together
+# # and save result in the result_2 variable.
+#
+# number_1 = '5353256'
+# index = 0
+# result_2 = 1
+# while index < len(str(number_1)):
+#     result_2 *= int(str(number_1)[index])
+#     index += 1
+# print(result_2)
 
 
 # Enter a random number and save it in variable number_2. Then create code which will return
 # a number with digits of number_1 in reverse order. Save it in result_3 variable
 
-number_2 = None
-result_3 = None
+number_2 = '234235'
+index =len(str(number_2)) - 1
+result_3  = ''
+while index >= 0:
+    result_3 += str(number_2)[index]
+    index  -= 1
+print(result_3)
